89d3ca7409e3b2958ab87efe82443a26885813ca
Konstantin Myakshin <koc-dp@yandex.ru>
2016-12-15 00:51:49 +0200

    Fix compatibility with Magento 2.1.3

--- Block/Tab/Content/Config.php
+++ Block/Tab/Content/Config.php
@@ -2,25 +2,18 @@
 
 namespace ADM\QuickDevBar\Block\Tab\Content;
 
-use Magento\Framework\App\ObjectManager;
-use Magento\Framework\Interception\DefinitionInterface;
-
-
 class Config extends \ADM\QuickDevBar\Block\Tab\Panel
 {
-
     protected $_config_values;
 
-    /**
-     * @var \Magento\Framework\App\Config\ScopePool
-     */
-    protected $_scopePool;
+    protected $_appConfig;
 
-    public function __construct(\Magento\Framework\View\Element\Template\Context $context,
-                                   \Magento\Framework\App\Config\ScopePool $scopePool,
-                                   array $data = [])
-    {
-        $this->_scopePool = $scopePool;
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Framework\App\Config $appConfig,
+        array $data = []
+    ) {
+        $this->_appConfig = $appConfig;
 
         parent::__construct($context, $data);
     }
@@ -32,36 +25,25 @@ class Config extends \ADM\QuickDevBar\Block\Tab\Panel
 
     public function getConfigValues()
     {
-        if(is_null($this->_config_values)) {
-            $this->_config_values =  [];
-            $scopePool = ObjectManager::getInstance()->get('Magento\Framework\App\Config\ScopePool');
-
-            $reflection = new \ReflectionClass($scopePool);
-
-            $scope = $reflection->getProperty('_scopes');
-            $scope->setAccessible(true);
-            $scope = $scope->getValue($scopePool);
-
-            $scope= current($scope);
-
-            $this->_config_values = $this->_buildFlatConfig($scope->getSource());
+        if (is_null($this->_config_values)) {
+            $this->_config_values = $this->_buildFlatConfig($this->_appConfig->getValue());
         }
 
         return $this->_config_values;
     }
 
-    protected function _buildFlatConfig($scope, $path='')
+    protected function _buildFlatConfig($scope, $path = '')
     {
         $flatConfig = [];
-        if(is_array($scope)) {
-            foreach($scope as $scopeKey=>$scopeValue) {
-                $buildedPath = !empty($path) ? $path . '/' .$scopeKey : $scopeKey;
+        if (is_array($scope)) {
+            foreach ($scope as $scopeKey => $scopeValue) {
+                $buildedPath = !empty($path) ? $path.'/'.$scopeKey : $scopeKey;
                 $flatConfig = array_merge($flatConfig, $this->_buildFlatConfig($scopeValue, $buildedPath));
             }
         } else {
-            $flatConfig[$path] = ['path'=>$path, 'value'=>$scope];
+            $flatConfig[$path] = ['path' => $path, 'value' => $scope];
         }
+
         return $flatConfig;
     }
-
 }
