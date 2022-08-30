ed9afa9a95baecee5a773404765eea28819d040e
Vincent Pietri <vincent.pietri@gmail.com>
2022-08-30 23:24:37 +0200

    Compatibility 2.4.4, Closes #60, closes #62

--- view/base/requirejs-config.js
+++ view/base/requirejs-config.js
@@ -1,10 +1,10 @@
 var config = {
     paths: {
-        quickDevBar:  "ADM_QuickDevBar/js/quickdevbar",
-        filtertable:  "ADM_QuickDevBar/js/sunnywalker/jquery.filtertable.min",
-        metadata:  "ADM_QuickDevBar/js/tablesorter/jquery.metadata",
-        tablesorter:  "ADM_QuickDevBar/js/tablesorter/jquery.tablesorter.min",
-        jqueryTabs: ["jquery-ui-modules/tabs", "jquery/jquery-ui"]
+        quickDevBar:  'ADM_QuickDevBar/js/quickdevbar',
+        filtertable:  'ADM_QuickDevBar/js/sunnywalker/jquery.filtertable.min',
+        metadata:  'ADM_QuickDevBar/js/tablesorter/jquery.metadata',
+        tablesorter:  'ADM_QuickDevBar/js/tablesorter/jquery.tablesorter.min',
+        jqueryTabs: ['jquery-ui-modules/tabs', 'jquery/jquery-ui']
     },
     shim: {
         'quickDevBar': {
